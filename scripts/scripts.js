6d38d9dd5ace11b03be25988f3d87ca6292e86cc
David Singleton <davidjsingleton18@gmail.com>
2025-07-22 11:04:12 -0700

    Update hamburger menu click event to display mobile nav menu overlay as well

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,13 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu');
 const mobileNavMenu = document.querySelector('#mobile-nav-menu');
+const mobileNavOverlay = document.querySelector('#mobile-nav-overlay');
 
 hamburgerMenu.addEventListener('click', (e) => {
     if (mobileNavMenu.classList.contains('mobile-nav-menu--open')) {
         mobileNavMenu.classList.remove('mobile-nav-menu--open');
+        mobileNavOverlay.style.display = 'none';
     } else {
         mobileNavMenu.classList.add('mobile-nav-menu--open');
+        mobileNavOverlay.style.display = 'block';
     }
 });
