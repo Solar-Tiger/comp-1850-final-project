fbe24afb3efefa67b0026eb4f189e772eaefc048
Solar_Tiger <davidjsingleton30@yahoo.com>
2025-07-22 12:58:48 -0700

    Update event listeners to utilize opening and collapsing mobile nav menu correctly

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,22 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu');
+
 const mobileNavMenu = document.querySelector('#mobile-nav-menu');
 const mobileNavOverlay = document.querySelector('#mobile-nav-overlay');
+const documentBody = document.querySelector('body');
 
-hamburgerMenu.addEventListener('click', (e) => {
-    if (mobileNavMenu.classList.contains('mobile-nav-menu--open')) {
-        mobileNavMenu.classList.remove('mobile-nav-menu--open');
-        mobileNavOverlay.style.display = 'none';
-    } else {
-        mobileNavMenu.classList.add('mobile-nav-menu--open');
-        mobileNavOverlay.style.display = 'block';
-    }
+hamburgerMenu.addEventListener('click', () => {
+	mobileNavMenu.classList.add('mobile-nav-menu--open');
+	mobileNavOverlay.style.display = 'block';
+	documentBody.classList.add('overflow-body--hidden');
+});
+
+document.body.addEventListener('click', (e) => {
+	if (
+		e.target.classList.contains('mobile-nav-overlay') ||
+		e.target.classList.contains('close-x-icon')
+	) {
+		mobileNavMenu.classList.remove('mobile-nav-menu--open');
+		mobileNavOverlay.style.display = 'none';
+		documentBody.classList.remove('overflow-body--hidden');
+	}
 });
